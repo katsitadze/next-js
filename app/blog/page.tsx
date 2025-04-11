3cd4bb919d2788203482158bc1b75c3751e5a4bd
katsitadze <katsimariam@gmail.com>
2025-04-11 21:19:47 +0400

    Your message

---
+++ app/blog/page.tsx
@@ -0,0 +1,11 @@
+import BlogCard from "../components/BlogCard";
+
+
+export default function BlogPage() {
+  return (
+    <div>
+      <BlogCard title="React Routing" />
+      <BlogCard title="Next.js Nested Routes" />
+    </div>
+  )
+}
---
+++ app/blog/slug/page.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+import SlugReader from "@/app/components/SlugReader"
+
+
+
+export default function BlogPost() {
+  return <SlugReader />
+}
